d6c32589e9938f520e2714a65541050e6f3b2f7a
jhaind1 <92746541+jhaind1@users.noreply.github.com>
2021-10-18 12:36:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    snakeheadiculus.turn(Direction.Right, 45)
+    snakeheadiculus.turn(Direction.Right, 90)
 })
 let snakeheadiculus: game.LedSprite = null
 snakeheadiculus = game.createSprite(0, 0)
 let snack = game.createSprite(randint(0, 4), randint(0, 4))
 while (true) {
     snakeheadiculus.move(1)
-    basic.pause(500)
-}
-if (snakeheadiculus.isTouching(snack)) {
-    snack.delete()
+    basic.pause(200)
+    if (snakeheadiculus.isTouching(snack)) {
+        snack.set(LedSpriteProperty.X, randint(0, 4))
+        snack.set(LedSpriteProperty.Y, randint(0, 4))
+    }
+    if (true) {
+    	
+    }
 }
 basic.forever(function () {
 	
