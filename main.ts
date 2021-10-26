be5b11663c8671a072bae1c034be1cfe01aead0e
jhaind1 <92746541+jhaind1@users.noreply.github.com>
2021-10-25 21:56:58 -0500

    Snake#3

--- main.ts
+++ main.ts
@@ -4,6 +4,8 @@ input.onButtonPressed(Button.A, function () {
 let snakeheadiculus: game.LedSprite = null
 snakeheadiculus = game.createSprite(0, 0)
 let snack = game.createSprite(randint(0, 4), randint(0, 4))
+let snake2 = game.createSprite(snakeheadiculus.get(LedSpriteProperty.X), snakeheadiculus.get(LedSpriteProperty.Y))
+snake2.set(LedSpriteProperty.Brightness, 0)
 while (true) {
     snakeheadiculus.move(1)
     basic.pause(200)
@@ -12,7 +14,23 @@ while (true) {
         snack.set(LedSpriteProperty.Y, randint(0, 4))
         game.addScore(1)
     }
+    if (game.score() >= 1) {
+        if (snakeheadiculus.get(LedSpriteProperty.Direction) == 0) {
+            snake2.set(LedSpriteProperty.X, snakeheadiculus.get(LedSpriteProperty.X))
+            snake2.set(LedSpriteProperty.Y, snakeheadiculus.get(LedSpriteProperty.Y) + 1)
+        }
+        if (snakeheadiculus.get(LedSpriteProperty.Direction) == 90) {
+            snake2.set(LedSpriteProperty.X, snakeheadiculus.get(LedSpriteProperty.X) - 1)
+            snake2.set(LedSpriteProperty.Y, snakeheadiculus.get(LedSpriteProperty.Y))
+        }
+        if (snakeheadiculus.get(LedSpriteProperty.Direction) == -90) {
+            snake2.set(LedSpriteProperty.X, snakeheadiculus.get(LedSpriteProperty.X) + 1)
+            snake2.set(LedSpriteProperty.Y, snakeheadiculus.get(LedSpriteProperty.Y))
+        }
+        if (snakeheadiculus.get(LedSpriteProperty.Direction) == 180) {
+            snake2.set(LedSpriteProperty.X, snakeheadiculus.get(LedSpriteProperty.X) - 1)
+            snake2.set(LedSpriteProperty.Y, snakeheadiculus.get(LedSpriteProperty.Y))
+        }
+        snake2.set(LedSpriteProperty.Brightness, 10000)
+    }
 }
-basic.forever(function () {
-	
-})
